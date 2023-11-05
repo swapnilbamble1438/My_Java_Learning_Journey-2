a1fdb5e8f63926c750be243808624febeced7eec
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-06 00:28:36 +0530

    MyJJ2-4

---
+++ B2iii_Servlet_WorkingExample/src/main/java/one/check.java
@@ -0,0 +1,38 @@
+package one;
+
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+
+@WebServlet("/check")
+public class check extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String uname = request.getParameter("uname");
+		String pass = request.getParameter("pass");
+		
+		if(uname.equals("Swapnil") && pass.equals("1234"))
+		{
+			response.sendRedirect("welcome.jsp");
+		}
+		else 
+		{
+			response.sendRedirect("error.jsp");
+		}
+	}
+
+}
